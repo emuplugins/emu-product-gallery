2a65385c838462fcdc8a581256612b8d49ab39d1
tonnynho2004 <147978343+tonnynho2004@users.noreply.github.com>
2025-02-11 07:42:13 -0300

    Create emu_product_gallery.php

---
+++ emu_product_gallery.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Plugin Name: Emu Product Gallery
+Plugin URI: https://example.com/emu-product-gallery
+Description: Um plugin para exibir sliders de galeria de imagens e vídeos do YouTube.
+Version: 1.0
+Author: Angard Agency
+Author URI: https://aganrdagency.com
+*/
+
+// Enfileirando o CSS e o JS do plugin
+function emu_product_gallery_enqueue_assets() {
+    // Enfileira o CSS do Swiper
+    wp_enqueue_style('swiper-style', 'https://unpkg.com/swiper/swiper-bundle.min.css', array(), null);
+
+    // Enfileira o CSS do seu plugin
+    wp_enqueue_style('emu-product-gallery-style', plugin_dir_url(__FILE__) . 'assets/css/style.css');
+
+    // Enfileira o JS do Swiper
+    wp_enqueue_script('swiper-script', 'https://unpkg.com/swiper/swiper-bundle.min.js', array(), null, true);
+
+    // Enfileira o JS do seu plugin
+    wp_enqueue_script('emu-product-gallery-script', plugin_dir_url(__FILE__) . 'assets/js/script.js', array('swiper-script', 'jquery'), null, true);
+}
+add_action('wp_enqueue_scripts', 'emu_product_gallery_enqueue_assets');
+
+// Incluindo o shortcode do slider
+function emu_product_gallery_include_slider_shortcode() {
+    // Inclui o arquivo que contém o shortcode
+    if (file_exists(plugin_dir_path(__FILE__) . 'includes/slider_template.php')) {
+        require_once plugin_dir_path(__FILE__) . 'includes/slider_template.php';
+    }
+}
+
+add_action('init', 'emu_product_gallery_include_slider_shortcode');
+
