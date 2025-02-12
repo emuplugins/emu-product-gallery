f28d30eb992acf7d5b62bbac7a78158f6cd3604a
tonnynho2004 <147978343+tonnynho2004@users.noreply.github.com>
2025-02-12 02:53:56 -0300

    Update emu-product-gallery.php

--- emu-product-gallery.php
+++ emu-product-gallery.php
@@ -9,6 +9,7 @@ Author URI: https://aganrdagency.com
 */
 require_once plugin_dir_path(__FILE__) . 'update-handler.php';
 require_once plugin_dir_path(__FILE__) . 'includes/metaboxes.php';
+require_once plugin_dir_path(__FILE__) . 'includes/option_page.php';
 // Enfileirando o CSS e o JS do plugin
 function emu_product_gallery_enqueue_assets() {
     // Enfileira o CSS do Swiper
