9753ea68bfea61526ac12588ed6b4291b5e65838
Emu Plugins <147978343+emuplugins@users.noreply.github.com>
2025-02-14 05:05:55 -0300

    Update emu-product-gallery.php

--- emu-product-gallery.php
+++ emu-product-gallery.php
@@ -11,7 +11,12 @@ Author URI: https://aganrdagency.com
 if (!defined('ABSPATH')) {
     exit;
 }
-$plugin_slug = 'emu-product-gallery'; // for the update_handler.php
+// Obtendo o nome do plugin e removendo o sufixo '-main'
+$plugin_slug = rtrim(plugin_dir_path(__FILE__), '/') ;
+$plugin_slug = basename($plugin_slug);
+if (substr($plugin_slug, -5) === '-main') {
+    $plugin_slug = substr($plugin_slug, 0, -5); // Remove o sufixo '-main'
+}
 require_once plugin_dir_path(__FILE__) . 'update-handler.php';
 require_once plugin_dir_path(__FILE__) . 'includes/metaboxes.php';
 require_once plugin_dir_path(__FILE__) . 'includes/option_page.php';
