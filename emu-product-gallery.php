dcfce5e15a6faa6b5fc68036abac65b7387986be
emuplugins <tonnysantanasantos@gmail.com>
2025-02-16 11:16:09 -0300

    a

--- emu-product-gallery.php
+++ emu-product-gallery.php
@@ -10,19 +10,32 @@ Author URI: https://aganrdagency.com
 
 if (!defined('ABSPATH')) exit;
 
-require_once plugin_dir_path(__FILE__) . 'update-handler.php';
+
 require_once plugin_dir_path(__FILE__) . 'includes/metaboxes.php';
 require_once plugin_dir_path(__FILE__) . 'includes/option_page.php';
 
-// Configuração do auto-update para o próprio plugin
-$plugin_slug = basename(__DIR__);
-if (substr($plugin_slug, -5) === '-main') {
-    $plugin_slug = substr($plugin_slug, 0, -5);
+// Carrega funções de atualização apenas na página de atualizações do wordpress
+
+function emu_load_on_update_pages() {
+    global $pagenow;
+
+    $update_pages = ['update-core.php', 'update.php', 'plugins.php', 'themes.php'];
+
+    if (in_array($pagenow, $update_pages)) {
+
+        require_once plugin_dir_path(__FILE__) . 'update-handler.php';
+        // Configuração do auto-update para o próprio plugin
+        $plugin_slug = basename(__DIR__);
+        if (substr($plugin_slug, -5) === '-main') {
+            $plugin_slug = substr($plugin_slug, 0, -5);
+        }
+        $self_plugin_dir = basename(__DIR__);
+
+        new Emu_Updater($plugin_slug, $self_plugin_dir);
+        add_action('upgrader_process_complete', 'emu_handle_plugin_update', 10, 2);
+
+    }
 }
-$self_plugin_dir = basename(__DIR__);
-
-new Emu_Updater($plugin_slug, $self_plugin_dir);
-
 
 // Enqueueing plugin CSS and JS
 function emu_product_gallery_enqueue_assets() {
@@ -50,3 +63,4 @@ function emu_product_gallery_include_slider_shortcode() {
 }
 
 add_action('init', 'emu_product_gallery_include_slider_shortcode');
+add_action('admin_init', 'emu_load_on_update_pages');
